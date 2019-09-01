c02dc073a88818d44f90ebbf85ea39b924f1b53d
Jeremy Black <Zechnophobe@gmail.com>
2019-08-31 17:38:01 -0700

    Add files via upload
    Initial commit with basic food/wood collection

---
+++ ControlButton.js
@@ -0,0 +1,80 @@
+class ControlButton {
+	
+	constructor(game) {
+		this.game = game;
+		this.config()
+		this.setup()
+	}
+	
+	config() {
+		this.cost = [];
+		this.name = "";
+		this.isVisible = false;
+		this.element = ""
+	}
+	
+	setup() {
+		this.element.on('click', () => {
+				this.onPress();
+		})
+	}
+	
+	shouldActivate() {
+		return true;
+	}
+	
+	render() {
+		if (this.shouldActivate()) {
+			this.isVisible = true;
+		}
+		this.element.text(this.name);
+		if (this.isVisible) {
+			this.element.show();
+		} else {
+			this.element.hide();
+		}
+	}
+	
+	
+	onPress() {
+		this.render();
+	}
+
+}
+
+
+class ForageButton extends ControlButton {
+	config() {
+		this.cost = [];
+		this.name = "Forage For Food";
+		this.requirement = [];
+		this.isVisible = true;
+		this.element = $("#forage-button");
+	}
+	
+	onPress() {
+		this.game.food += 1;
+		super.onPress()
+	}
+	
+}
+
+class ChopWoodButton extends ControlButton {
+	config() {
+		this.cost = [];
+		this.name = "Chop Wood";
+		this.requirement = ['hunger is full'];
+		this.isVisible = false;
+		this.element = $("#chop-wood-button");
+	}
+	
+	onPress() {
+		this.game.wood += 1;
+		super.onPress()
+	}
+	
+	shouldActivate() {
+		return this.game.food >= 10;
+	}
+	
+}
---
+++ Game.js
@@ -0,0 +1,7 @@
+class Game {
+	constructor(){
+		this.food = 0;
+		this.wood = 0;
+		this.friends = 0;
+	}
+}
